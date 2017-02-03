a2f617cd6fb3e301ca9e5faca9ec3607ce5d0c49
Tjier Salazar <tjier.salazarvaros@gmail.com>
2017-02-02 21:02:26 -0300

    probando las directivas ng-repeat, ng-model, ng-click

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,21 @@
 const app = angular.module("MyApp", []);
 
 app.controller("MyController", ($scope) =>{
-  $scope.nombre = "Tjier"
+  $scope.titulo = "Caja de Comentarios";
+  $scope.nuevoComentario = {};
+  $scope.comentarios = [
+    {
+      mensaje: "probando ng-repeat",
+      usuario: "Tjier"
+    },
+    {
+      mensaje: "probando ng-model",
+      usuario: "Tjier-2"
+    }
+  ];
+  $scope.agregarComentario = () => {
+    $scope.comentarios.push($scope.nuevoComentario)
+    $scope.nuevoComentario = {}
+  };
 
 });
