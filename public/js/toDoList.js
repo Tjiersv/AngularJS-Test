75b409e874f77d8e06d804a1169c0f28a2b93cc7
Tjier Salazar <tjier.salazarvaros@gmail.com>
2017-02-06 00:16:59 -0300

    se realizo un formulario para agregar una actividad por realizar. y se almaceno en el localstorage

--- public/js/toDoList.js
+++ public/js/toDoList.js
@@ -3,4 +3,21 @@
 const app = angular.module('ToDoList', ['LocalStorageModule'])
 
 app.controller('ToDoController', ($scope, localStorageService) => {
+
+  if (localStorageService.get("angular-todolist")) {
+    $scope.todo = localStorageService.get("angular-todolist")
+  }else{
+    $scope.todo = []
+  }
+  /*
+    {
+      descripcion: 'Terminar el curso de Angular',
+      fecha: '3-03-17 2:00pm'
+    }
+  */
+  $scope.addActividad = () => {
+    $scope.todo.push($scope.newActividad)
+    $scope.newActividad = {}
+    localStorageService.set("angular-todolist", $scope.todo)
+  }
 });
