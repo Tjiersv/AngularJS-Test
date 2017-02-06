0266bd43660341caef8b8bc1025c7ece65c6cac7
Tjier Salazar <tjier.salazarvaros@gmail.com>
2017-02-05 21:55:42 -0300

    se añadio un formulario para agregar un titulo y un texto mediante el metodo post de http

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -2,17 +2,35 @@
 
 angular.module("MyAjax", [])
   .controller("AjaxController", ($scope, $http) => {
-    $scope.datas = []
+
+    $scope.datas = [];
+    $scope.newPost = {};
+
+//  Obtener Datos
     $http.get("https://jsonplaceholder.typicode.com/posts")
+    .then((success)=>{
+      $scope.datas = success.data;
+      console.log(success);
+    },(err)=>{
+      console.log(`Error ${err} al obtener la informacion del servidor`);
+    });
+
+//  Funcion Añadir Post
+    $scope.addPost = function () {
+      console.log("Inicio POST");
+      console.log($scope.newPost.title);
+      $http.post("https://jsonplaceholder.typicode.com/posts", {
+          title : $scope.newPost.title,
+          body  : $scope.newPost.body,
+          userId: 1
+      })
       .then((success)=>{
-        $scope.datas = success.data;
-        console.log(success);
-      },(err)=>{
-        console.log(`Error ${err} al obtener la informacion del servidor`);
-      });
-    $http.post("https://jsonplaceholder.typicode.com/posts")
-      .then((success)=>{
+        console.log("Exito!");
+        $scope.datas.push($scope.newPost);
+        $scope.newPost = {};
       },(err)=>{
         console.log(`Error ${err}`);
       });
-  })
+    };
+
+  });
