6ed3ff7674e8912cf7e05907773aacc413394337
Tjier Salazar <tjier.salazarvaros@gmail.com>
2017-02-08 12:49:48 -0300

    se trabajó con  y child, mediante le cual el controlador hijo hereda el atributo del controlador padre

--- public/js/child.js
+++ public/js/child.js
@@ -1,4 +1,12 @@
-angular.module('MyChild', [])
-.controller('MyChildController',($scope)=>{
-  $scope.title = "Probando Child"
-})
+angular.module('MyApp', [])
+  .run(($rootScope) => {
+    $rootScope.nombre = "Probando rootScope"
+  })
+  .controller('MyController',($scope)=>{
+    $scope.title = "Probando rootScope y Chils"
+
+    $scope.nombre = "Probando $rS"
+  })
+  .controller('ChildController', () => {
+
+  })
